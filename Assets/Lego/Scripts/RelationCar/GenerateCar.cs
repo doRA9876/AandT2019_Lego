bb8ba059f97b098c975139025f0c7162397c29b2
doRA <dev9876dora@gmail.com>
2019-07-08 01:58:46 +0900

    [add] 車の種類を追加

--- Assets/Lego/Scripts/RelationCar/GenerateCar.cs
+++ Assets/Lego/Scripts/RelationCar/GenerateCar.cs
@@ -4,27 +4,51 @@ using UnityEngine;
 
 public class GenerateCar : MonoBehaviour
 {
-  public static GameObject car;
-  float elapsedTime = 10f;
+  public static GameObject skyCar_Red, policeCar, car_Sedan;
+  float elapsedTime = 0f;
+  float nextGenerateTiming = 0f;
 
   void Start()
   {
-    car = (GameObject)Resources.Load("Car/SkyCar");
+    skyCar_Red = (GameObject)Resources.Load("Car/SkyCar_Red");
+    policeCar = (GameObject)Resources.Load("Car/PoliceCar");
+    car_Sedan = (GameObject)Resources.Load("Car/Car_Sedan");
   }
 
   void Update()
   {
     elapsedTime += Time.deltaTime;
 
-    if(elapsedTime > 10f)
+    if(elapsedTime > nextGenerateTiming)
     {
       elapsedTime = 0;
+      nextGenerateTiming = Random.Range(5f, 10f);
       ExecuteGeneration();
     }
   }
 
   public void ExecuteGeneration()
   {
-    Instantiate(car, gameObject.transform.position, Quaternion.identity);
+    int random = Random.Range(0, 3);
+    GameObject obj;
+    switch (random)
+    {
+      case 0:
+        obj = skyCar_Red;
+        break;
+
+      case 1:
+        obj = policeCar;
+        break;
+      
+      case 2:
+        obj = car_Sedan;
+        break;
+
+      default:
+        obj = skyCar_Red;
+        break;
+    }
+    Instantiate(obj, gameObject.transform.position, Quaternion.identity);
   }
 }
--- Assets/Lego/Scripts/RelationCar/MoveCar.cs
+++ Assets/Lego/Scripts/RelationCar/MoveCar.cs
@@ -22,7 +22,7 @@ class MovingCar
     Vector3 direction = 2 * ((t - 1) * wayPoint[0] + (1 - 2 * t) * wayPoint[1] + wayPoint[2] * t);
     car.transform.rotation = Quaternion.LookRotation(direction);
   }
-  
+
   Vector3 Bezier3D(Vector3 p1, Vector3 p2, float t)
   {
     return (1 - t) * p1 + t * p2;
@@ -37,7 +37,7 @@ public class MoveCar : MonoBehaviour
   {
     foreach (var item in movingCar)
     {
-      if(item.car == collider.gameObject) return;
+      if (item.car == collider.gameObject) return;
     }
     MovingCar car = new MovingCar(wayPoint, collider.gameObject);
     movingCar.Add(car);
@@ -45,15 +45,28 @@ public class MoveCar : MonoBehaviour
 
   void Update()
   {
+    List<MovingCar> removeList = new List<MovingCar>();
+
     foreach (var item in movingCar)
     {
+      if(item.car == null)
+      {
+        removeList.Add(item);
+        continue;
+      }
+
       item.timeElapsed += Time.deltaTime;
       item.Move();
 
-      if(item.timeElapsed > LegoData.CAR_SPEED)
+      if (item.timeElapsed > LegoData.CAR_SPEED)
       {
-        movingCar.Remove(item);
+        removeList.Add(item);
       }
+    }
+
+    foreach (var item in removeList)
+    {
+      movingCar.Remove(item);
     }
   }
 }
