e477154bda1cabfe69f9e75c000ccdcc48302f48
doRA <dev9876dora@gmail.com>
2019-07-08 22:15:12 +0900

    [add] 公園追加、川の音を変更

--- Assets/Lego/Scripts/RelationLego/LegoEnum.cs
+++ Assets/Lego/Scripts/RelationLego/LegoEnum.cs
@@ -20,7 +20,7 @@
   public enum LandscapeType_Details
   {                                                                                                           //LandscapeType_Overview
     House, Shop, Skyscraper,                                                                    //Building
-    River_Straight, River_Curve, River_Intersection_T, Sea, Pond,                                        //Water
+    River_Straight, River_Curve, River_Intersection_T, River_Intersection_X, Sea, Pond,                                        //Water
     Forest, Park,                                                                                 //Nature
     Road_Straight, Road_Curve, Road_Intersection_T, Road_Intersection_X, Road_Stop, Road_CrossWalk, Bridge,   //Road
     Space                                                                                                     //Spaces
